9017a8c17fbc75602fb396edf07448449cd266fc
David Leonhart <leozilla@gmail.com>
2017-08-19 12:14:45 +0200

    Added copy command

--- configure-minikube-jenkins.sh
+++ configure-minikube-jenkins.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cp -R .minikube ~/.minikube/
+cp -R .minikube /var/lib/jenkins/
+cp -R .minikube /home/jenkins/
 
 kubectl config set clusters.cluster.certificate-authority ~/.minikube/ca.crt
 kubectl config set clusters.cluster.server https://192.168.99.101:8443
