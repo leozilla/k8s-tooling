f8e2ca80e5f93c49312613c9c5cc1502df8432a8
David Leonhart <leozilla@gmail.com>
2017-08-19 11:50:04 +0200

    Added files

---
+++ configure-kube.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cp -R ./minikube ~/.minikube/
+
+kubectl config set clusters.cluster.certificate-authority ~/.minikube/ca.crt
+kubectl config set clusters.cluster.server https://192.168.99.101:8443
+
+kubectl config set contexts.context.cluster minikube
+kubectl config set contexts.context.user minikube
+
+kubectl config set current.context minikube
+
+kubectl config set users.name minikube
+kubectl config set users.user.client-certificate ~/.minikube/apiserver.crt
+kubectl config set users.user.client-key ~/.minikube/apiserver.key
