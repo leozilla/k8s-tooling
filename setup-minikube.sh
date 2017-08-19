019f4ba57177ed9dbc84d3393b61f68edf53bef2
David Leonhart <leozilla@gmail.com>
2017-08-19 11:02:55 +0200

    Dont start minikube if already running

--- setup-minikube.sh
+++ setup-minikube.sh
@@ -23,7 +23,9 @@ mkdir $HOME/.kube || true
 touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
-minikube start
+if [ "$(minikube status | head -n 1 )" != "minikube: Running" ]; then
+    minikube start
+fi
 
 # this for loop waits until kubectl can access the api server that minikube has created
 for i in {1..150} # timeout for 5 minutes
